b344ab76684e5d278e740d23b1fe8b64fc224e9d
LuisHCh <huanca.luis@pucp.pe>
2022-12-29 18:21:18 -0500

    correcion de la validacion

--- src/js/login.js
+++ src/js/login.js
@@ -4,12 +4,9 @@ import validate from "./validation.js";
 const formLogin = document.getElementById("form");
 const email = document.getElementById("exampleInputEmail1");
 const password = document.getElementById("exampleInputPassword1");
-console.log("PRUEBAAAA")
 
 let loginUser = async (event) => {
     event.preventDefault();
-    console.log("email", email.value)
-    console.log("password", password.value)
     let validated = validate([email.value, password.value]);
     if (validated) {
         let response = await fetch("http://127.0.0.1:8000/users/login/" , {
@@ -24,11 +21,10 @@ let loginUser = async (event) => {
         });
         let data = await response.json();
         if (response.status === 200) {
-            
             let { tokens, data: user } = data
             localStorage.setItem("authTokens", JSON.stringify(tokens));
             localStorage.setItem("user", JSON.stringify(user));
-            window.location.replace("../templates/index.html");
+            window.location.replace("../../templates/index.html");
             
             
         } else {
